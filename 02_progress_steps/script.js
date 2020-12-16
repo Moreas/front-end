3b0025bfdfb2d2d29adbeac052c0c870bcc8ea9d
EC2 Default User <ec2-user@ip-172-31-37-104.us-east-2.compute.internal>
2020-12-16 21:26:55 +0000

    project 02 completed

--- 02_progress_steps/script.js
+++ 02_progress_steps/script.js
@@ -12,17 +12,18 @@ next.addEventListener('click', () => {
         currentActive = circles.length;
     }
 
+update();
 })
 
 
 prev.addEventListener('click', () => {
     currentActive--
 
-    if(currentActive , 1) {
+    if(currentActive < 1) {
         currentActive = 1;
     }
 
-    update();
+update();
 })
 
 function update() {
@@ -34,9 +35,16 @@ function update() {
         }
     })
 
-    const actives = document.querySelector('active');
-
+    const actives = document.querySelectorAll('.active');
     progress.style.width = (actives.length - 1) / (circles.length -1) * 100 + "%";
-}
 
-console.log(currentActive);
+    if(currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length){
+       next.disabled = true;
+   } else {
+       prev.disabled = false;
+       next.disabled = false;
+   }
+}
+   
